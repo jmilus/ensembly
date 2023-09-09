da7cdac1bba78024372c09d8a3c99e1c376c09e3
Joshua Milus <joshua@dbworks.com>
2023-09-08 19:46:44 -0600

    modified environment console logs

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ const getURL = () => {
         return 'http://localhost:3000'
     } else {
         console.log("running a production build...")
+        console.log("process.env.NEXT_PUBLIC_SITE_URL:", process.env.NEXT_PUBLIC_SITE_URL)
         let url =
             process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
             process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
